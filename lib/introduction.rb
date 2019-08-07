9cf63ca5e1095524632804170a324b62f6c0eae1
Sharrieff ross <crross@dons.usfca.edu>
2019-08-07 21:56:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,11 +10,11 @@ end
    puts "Hi, my name is #{name} and I am learning to program in #{language}."
  end
  
- def introduction_with_Language_optional(name, Language = "Ruby")
+ def introduction_with_language_optional(name, Language = "Ruby")
    puts "Hi my name is #{name} and I am learning to program in #{Language}."
  end
  
- def introduction_with_Language_optional(name, Language = "Ruby")
+ def introduction_with_language_optional(name, Language = "Ruby")
    puts "Hi my name is #{name} and I am learning to program in #{Language}."
  end
  
